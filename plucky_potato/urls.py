f1c40f35d6f5e0027a58e2e01bc712a01de83c4e
Americo <ame.zuz@gmail.com>
2018-11-30 23:08:29 -0800

    Set-up api and home page paths

--- plucky_potato/urls.py
+++ plucky_potato/urls.py
@@ -15,15 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import include
 from tastypie.api import Api
 from evently.resources import EventsResource, LocationsResource
+from evently.views import IndexView
 
 v1_api = Api(api_name='v1')
 v1_api.register(EventsResource())
 v1_api.register(LocationsResource())
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
-    url(r'^api/', include(v1_api.urls)),
+    path('api/', include(v1_api.urls)),
 ]
