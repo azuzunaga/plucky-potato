3f265f13b23fa622596484e189f59a6979bd4053
Americo <ame.zuz@gmail.com>
2018-12-01 14:12:14 -0800

    Add registration and pagination

--- evently/static/js/Home.js
+++ evently/static/js/Home.js
@@ -10,64 +10,109 @@ class Nav extends React.Component {
   }
 }
 
-const Event = props => {
-  const {
-    name,
-    status,
-    start_date,
-    participant_count,
-    registered,
-    description,
-  } = props.event;
-
-  return (
-    <div>
-      <h4>{name}</h4>
-      <p>{description}</p>
-      <section>
-        <p>Start date and time: {start_date}</p>
-        <p>Status: {status}</p>
-        <p>Participants: {participant_count}</p>
-        <button>{registered ? 'Unregister' : 'Register'}</button>
-      </section>
-    </div>
-  );
-};
+class Event extends React.Component {
+  render() {
+    const {
+      id,
+      name,
+      status,
+      start_date,
+      participant_count,
+      registered,
+      description,
+    } = this.props.event;
+    return (
+      <div>
+        <h4>{name}</h4>
+        <p>{description}</p>
+        <section>
+          <p>Start date and time: {start_date}</p>
+          <p>Status: {status}</p>
+          <p>Participants: {participant_count}</p>
+          <button
+            onClick={() => this.props.onClick(id, registered)}
+            data-eventid={id}
+          >
+            {registered ? 'Unregister' : 'Register'}
+          </button>
+        </section>
+      </div>
+    );
+  }
+}
 
 class Events extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       events: [],
-      numPages: 0,
+      page: 0,
+      pages: 0,
       next: '',
       previous: '',
+      url: '',
+      error: '',
     };
+    this.register = this.register.bind(this);
   }
 
   componentDidMount() {
     this.fetchPage('api/v1/events');
   }
 
-  fetchPage(page) {
-    fetch(page)
+  fetchPage(url) {
+    fetch(url)
       .then(res => res.json())
       .then(res => {
-        const { limit, next, previous, total_count } = res.meta;
+        const { limit, offset, next, previous, total_count } = res.meta;
         this.setState({
           events: res.objects,
-          numPages: total_count % limit,
+          page: parseInt(offset / limit) + 1,
+          pages: total_count % limit,
           next,
           previous,
+          url,
         });
       });
+  }
+
+  register(eventId, registered) {
+    // This takes the current value of event.registered and does a POST
+    // or DELETE request
+    const method = registered ? 'DELETE' : 'POST';
+    const url = `api/v1/events/${eventId}/register/`;
+    fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    })
+      .then(() => this.fetchPage(this.state.url))
+      .catch(error => this.setState({ error }));
   }
 
   render() {
     return (
       <div>
+        <section>
+          <button
+            onClick={() => this.fetchPage(this.state.previous)}
+            disabled={!this.state.previous}
+          >
+            ← Prev
+          </button>
+          <p>
+            Page {this.state.page} of {this.state.pages}
+          </p>
+          <button
+            onClick={() => this.fetchPage(this.state.next)}
+            disabled={!this.state.next}
+          >
+            Next →
+          </button>
+        </section>
         {this.state.events.map(event => (
-          <Event event={event} />
+          <Event event={event} key={event.id} onClick={this.register} />
         ))}
       </div>
     );
