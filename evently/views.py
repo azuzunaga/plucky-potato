ea2462bce996dc5884bc2eda61e3510225a253de
Americo <ame.zuz@gmail.com>
2018-11-30 23:09:05 -0800

    Load home page template

--- evently/views.py
+++ evently/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+from django.views.generic.base import TemplateView
+
+
+class IndexView(TemplateView):
+
+    template_name = 'home.html'
