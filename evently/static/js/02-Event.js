4235537155114339ef1766697efcffd94712e46b
Americo <ame.zuz@gmail.com>
2018-12-02 17:30:29 -0800

    Add css and visual indication of registration

--- evently/static/js/02-Event.js
+++ evently/static/js/02-Event.js
@@ -1,10 +1,25 @@
 'use strict';
 
 class Event extends React.Component {
+  formatDateTime = dateTimeString => {
+    const jsDateTime = new Date(dateTimeString + 'z');
+    const options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      timeZoneName: 'short',
+    };
+    return jsDateTime.toLocaleString('en-us', options);
+  };
+
   render() {
     const {
       id,
       name,
+      short_location,
       status,
       start_date,
       participant_count,
@@ -13,15 +28,30 @@ class Event extends React.Component {
     } = this.props.event;
     return (
       <div className="event">
-        <h4>{name}</h4>
+        <header>
+          <h4>{name}</h4>
+          <h5>{short_location}</h5>
+          <hr />
+        </header>
         <p>{description}</p>
+        <hr />
         <section>
-          <p>Start date and time: {start_date}</p>
-          <p>Status: {status}</p>
-          <p>Participants: {participant_count}</p>
+          <span>
+            <strong>When:</strong>
+            {this.formatDateTime(start_date)}
+          </span>
+          <span>
+            <strong>Status:</strong>
+            {status}
+          </span>
+          <span>
+            <strong>Participants:</strong>
+            {participant_count}
+          </span>
           <button
-            onClick={() => this.props.onClick(id, registered)}
+            onClick={e => this.props.onClick(id, registered)}
             data-eventid={id}
+            className={registered ? 'registered' : undefined}
           >
             {registered ? 'Unregister' : 'Register'}
           </button>
